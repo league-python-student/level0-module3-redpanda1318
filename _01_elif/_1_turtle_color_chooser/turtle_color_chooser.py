328a4cbbf21043746d97053fdb684d5c3a0aa507
caris <yeahredpanda@gmail.com>
2021-09-29 01:24:25 +0000

    crazy cat lady turtle color chooser

--- _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
+++ _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
@@ -15,15 +15,66 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # TODO 1) Create a new Turtle
+    t = turtle.Turtle()
     #      2) Make the turtle draw a shape (this will take more than one line
     #         of code)
+    turtle.forward(150)
+    turtle.right(90)
+    turtle.forward(80)
+    turtle.right(90)
+    turtle.forward(150)
+    turtle.right(90)
+    turtle.forward(80)
+
     #      3) Set the pen width to 10
+    t.width(10)
+
     #      4) Ask the user what color pen they would like to draw with
+    for i in range(20):
+
+        color = simpledialog.askstring(title = 'colors',prompt = "What color would you like to draw with?")
+
+
+
     #      5) Use an if/else statement to set the pen color that the user
     #         requested
+        if color == "red":
+            turtle.pencolor('red')
+
+
+        elif color == "orange":
+            turtle.pencolor('orange')
+
+        elif color == "yellow":
+            turtle.pencolor('yellow')
+
+        elif color == "green":
+            turtle.pencolor('green')
+
+        elif color == "blue":
+            turtle.pencolor('blue')
+
+        elif color == "purple":
+            turtle.pencolor('purple')
+
+        elif color == "pink":
+            turtle.pencolor('pink')
+
     #      6) If the user doesn't enter anything, choose a random color
+        else:
+            turtle.pencolor(get_random_color())
+
+        turtle.forward(150)
+        turtle.right(90)
+        turtle.forward(80)
+        turtle.right(90)
+        turtle.forward(150)
+        turtle.right(90)
+        turtle.forward(80)
+
     #      7) Put a loop around your code so that you keep asking the user for
     #         more colors & drawing them
+
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
--- _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
+++ _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
@@ -11,12 +11,23 @@ def play_video(url):
 
 if __name__ == '__main__':
     # TODO 1) Make a new window variable, window = Tk()
+    window = Tk()
     #      2) Hide the window using the window's .withdraw() method
+    window.withdraw()
     #      3) Ask the user how many cats they have
+    cats = messagebox.askstring(title='stuff', prompt="How many cats do you have?")
     #      4) If they have 3 or more cats, tell them they are a crazy cat lady
+    if cats >= 3
+        print("You are a crazy cat lady")
+
     #      5) If they have less than 3 cats AND more than 0 cats, call the
     #         play_video function below to show them a cat video
+    if cats 0<=3
+        play_video()
     #      6) If they have 0 cats, show them a video of A Frog Sitting on a
     #         Bench Like a Human
+    if cats == 0
+
+
 
     pass
