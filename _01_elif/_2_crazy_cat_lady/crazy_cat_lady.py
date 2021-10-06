af0f512c86b0bf86658c8721b5b795427fe9edb7
caris <yeahredpanda@gmail.com>
2021-10-06 01:23:21 +0000

    are u happy, crazy cat lady

--- _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
+++ _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
@@ -15,18 +15,22 @@ if __name__ == '__main__':
     #      2) Hide the window using the window's .withdraw() method
     window.withdraw()
     #      3) Ask the user how many cats they have
-    cats = messagebox.askstring(title='stuff', prompt="How many cats do you have?")
+    cats = simpledialog.askstring(title='stuff', prompt="How many cats do you have?")
     #      4) If they have 3 or more cats, tell them they are a crazy cat lady
-    if cats >= 3
+    if int(cats) >= 3:
         print("You are a crazy cat lady")
 
     #      5) If they have less than 3 cats AND more than 0 cats, call the
     #         play_video function below to show them a cat video
-    if cats 0<=3
-        play_video()
+    elif int(cats) >0 and int(cats) <3:
+
+        play_video('https://www.youtube.com/watch?v=C6RUp21s6BQ')
     #      6) If they have 0 cats, show them a video of A Frog Sitting on a
     #         Bench Like a Human
-    if cats == 0
+    else:
+        play_video('https://www.youtube.com/watch?v=oj_yLBltPE8')
+
+
 
 
 
--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -4,5 +4,9 @@ from tkinter import Tk, simpledialog, messagebox
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
+    happy = simpledialog.askstring(title='question', prompt="Are You Happy?")
+    if happy == 'yes'
+        print("keep doing whatever you're doing")
+
 
     pass
