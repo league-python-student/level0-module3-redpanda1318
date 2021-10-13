f71f104b9f79327f46bf9c070f5cb38784e400fc
caris <yeahredpanda@gmail.com>
2021-10-13 01:09:44 +0000

    are u happy

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -1,12 +1,26 @@
 from tkinter import Tk, simpledialog, messagebox
 
+window = Tk()
+window.withdraw()
 
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
     happy = simpledialog.askstring(title='question', prompt="Are You Happy?")
-    if happy == 'yes'
-        print("keep doing whatever you're doing")
 
+    if happy == 'yes':
+        print("Keep doing whatever you're doing")
+
+
+    thing = simpledialog.askstring(title='not happy', prompt="Do You Want To Be Happy?")
+
+    if happy == 'no':
+        print(thing)
+
+    if thing == 'yes':
+        print("Change Something")
+
+    if thing == 'no':
+        print("Keep Doing Whatever You're Doing")
 
     pass
