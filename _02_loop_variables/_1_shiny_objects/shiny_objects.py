d8187b51cbe01ed58161f0013350a05952c51161
caris <yeahredpanda@gmail.com>
2021-10-27 01:30:05 +0000

    shiny objects, walk of fame

--- _02_loop_variables/_1_shiny_objects/shiny_objects.py
+++ _02_loop_variables/_1_shiny_objects/shiny_objects.py
@@ -1,23 +1,23 @@
 from tkinter import simpledialog, Tk
-from playsound import playsound
+
 
 can_play_sounds = False
 
 
 def play_mister_zee():
-    if can_play_sounds:
-        playsound('shiny-objects.wav')
-    else:
         print("Mister Zee requires shiny objects")
 
 
 def many_shiny_objects():
     # TODO 1) Call the method above to play Mister Zee
-    can_play_sounds
+
 
     # TODO 2) Ask the user how many shiny objects they want
-    print("How many shiny objects do you want?")
+    shiny = simpledialog.askstring(title = 'stuff', prompt="How many shiny objects do you want?")
     # TODO 3) Play the sound that many times
+    for x in range(int (shiny)):
+        play_mister_zee()
+
 
     pass
 
--- _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
+++ _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
@@ -7,8 +7,58 @@ if __name__ == '__main__':
     my_turtle.speed(100)
 
     # TODO 1) Set the X position of the turtle so that it starts on the left.
+    my_turtle.penup()
+    my_turtle.setx(-300)
 
     # TODO 2) Make the turtle draw a star shape. Hint: angle=144.
+    my_turtle.pendown()
+    for i in range(5):
+        my_turtle.forward(30)
+        my_turtle.left(144)
+
+    my_turtle.penup()
+    my_turtle.setx(-250)
+
+    my_turtle.pendown()
+    for i in range(5):
+        my_turtle.forward(30)
+        my_turtle.left(144)
+
+
+    my_turtle.penup()
+    my_turtle.setx(-200)
+
+    my_turtle.pendown()
+    for i in range(5):
+        my_turtle.forward(30)
+        my_turtle.left(144)
+
+
+    my_turtle.penup()
+    my_turtle.setx(-150)
+
+    my_turtle.pendown()
+    for i in range(5):
+        my_turtle.forward(30)
+        my_turtle.left(144)
+
+
+    my_turtle.penup()
+    my_turtle.setx(-100)
+
+    my_turtle.pendown()
+    for i in range(5):
+        my_turtle.forward(30)
+        my_turtle.left(144)
+
+
+    my_turtle.penup()
+    my_turtle.setx(-50)
+
+    my_turtle.pendown()
+    for i in range(5):
+        my_turtle.forward(30)
+        my_turtle.left(144)
 
     # TODO 3) Set the length of each line in the star to 30
 
